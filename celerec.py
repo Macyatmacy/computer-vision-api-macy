3264cb4fce492ae5e8db3108b7e8881c236011e2
Macyatmacy <60915903+Macyatmacy@users.noreply.github.com>
2021-05-14 21:18:12 +0800

    Add files via upload

---
+++ celerec.py
@@ -0,0 +1,38 @@
+import boto3
+import click
+
+@click.command(help="This tool does celebrity detection")
+@click.option('--file', prompt='I need the name of the file in the bucket',
+              help='This is the name of the file in the bucket')
+
+def recognize_celebrities(file):
+
+    
+    client=boto3.client('rekognition')
+
+    response = client.recognize_celebrities(
+        Image={
+            'S3Object': {
+                'Bucket': 'cvapimacy',
+                'Name': file,
+            },
+        },
+    )
+    click.echo(click.style(f"Detecting Celebrities for: {file}", fg="red"))
+
+    # print('Detected faces for ' + photo)    
+    for celebrity in response['CelebrityFaces']:
+        print ('Name: ' + celebrity['Name'])
+        print ('Id: ' + celebrity['Id'])
+        print ('Position:')
+        print ('   Left: ' + '{:.2f}'.format(celebrity['Face']['BoundingBox']['Height']))
+        print ('   Top: ' + '{:.2f}'.format(celebrity['Face']['BoundingBox']['Top']))
+        print ('Info')
+        for url in celebrity['Urls']:
+            print ('   ' + url)
+        
+    return len(response['CelebrityFaces'])
+
+if __name__ == '__main__':
+    # pylint: disable=E1120
+    recognize_celebrities()
